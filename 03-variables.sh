350400bfde7345d3469eb39e30fbb3c4f54e736e
Sriharsha Kalluru <skalluru@mac.local>
2021-10-20 08:35:22 +0530

    ss

---
+++ 03-variables.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+## If we assign a name to a set of data , then that is variable
+# SYNTAX: VAR=DATA
+
+# Number
+a=100
+# String
+b=abc
+
+# In bash shell and also by default there are no data types, Shell considers everything as string
+
+# Access the data in shell using $ character prefixing the variable name
+
+echo Value of a = $a
+echo Value of b = $b
