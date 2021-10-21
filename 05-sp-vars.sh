2f6a232ccf4a4b0766997c34020b05a6028a265d
Sriharsha Kalluru <skalluru@mac.local>
2021-10-21 07:49:59 +0530

    Update shell-scripting/

--- 05-sp-vars.sh
+++ 05-sp-vars.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
+# 0 is script name
 echo 0 = $0
+# First argument to script
 echo 1 = $1
+# Second argument to script
 echo 2 = $2
 
+## * and @ are giving all the argument
 echo "* = $*"
 echo "@ = $@"
+
+# Numner of values which are parsed
 echo "# = $#"
+
+
