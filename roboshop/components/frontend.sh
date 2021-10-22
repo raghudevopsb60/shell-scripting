db3ebd7b0f65dfc68ab470e514aef34b611ca8d9
Sriharsha Kalluru <skalluru@mac.local>
2021-10-22 08:13:44 +0530

    Update shell-scripting/

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -3,10 +3,12 @@
 echo -e "\e[1mInstalling Nginx\e[0m"
 yum install nginx -y
 
-exit
+echo -e "\e[1mEnabling Nginx\e[0m"
 systemctl enable nginx
+echo -e "\e[1mStarting Nginx\e[0m"
 systemctl start nginx
 
+exit
 curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
 
 cd /usr/share/nginx/html
