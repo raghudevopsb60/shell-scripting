92f455a589e9adf60277faae181125c978375824
Sriharsha Kalluru <skalluru@mac.local>
2021-10-22 09:06:45 +0530

    Update shell-scripting/

--- roboshop/components/mongodb.sh
+++ roboshop/components/mongodb.sh
@@ -2,4 +2,6 @@
 
 source components/common.sh
 
+Print "Download Repo"
 curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/roboshop-devops-project/mongodb/main/mongo.repo
+Stat $?
