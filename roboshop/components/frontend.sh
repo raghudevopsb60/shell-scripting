2e50c067cf9990e70627f07fd95215212a02022b
Sriharsha Kalluru <skalluru@mac.local>
2021-10-22 08:00:51 +0530

    Update shell-scripting/

---
+++ roboshop/components/frontend.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+yum install nginx -y
+
+systemctl enable nginx
+systemctl start nginx
+#Let's download the HTDOCS content and deploy under the Nginx path.
+
+curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
+
+cd /usr/share/nginx/html
+rm -rf *
+unzip /tmp/frontend.zip
+mv frontend-main/* .
+mv static/* .
+rm -rf frontend-master static README.md
+mv localhost.conf /etc/nginx/default.d/roboshop.conf
+systemctl restart nginx
