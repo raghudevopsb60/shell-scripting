36e45bd36665121a93fdebd9c161edb5250069fc
Sriharsha Kalluru <skalluru@mac.local>
2021-10-23 08:42:48 +0530

    Update shell-scripting/

--- roboshop/components/mongodb.sh
+++ roboshop/components/mongodb.sh
@@ -3,5 +3,36 @@
 source components/common.sh
 
 Print "Download Repo"
-curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/roboshop-devops-project/mongodb/main/mongo.repo
+curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/roboshop-devops-project/mongodb/main/mongo.repo &>>$LOG
 Stat $?
+
+Print "Install MongoDB"
+yum install -y mongodb-org &>>$LOG
+Stat $?
+
+Print "Update MongoDB Config"
+sed -i -e 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf &>>$LOG
+Stat $?
+
+Print "Start MongoDB"
+systemctl restart mongod &>>$LOG
+Stat $?
+
+Print "Enable MongoDB Service"
+systemctl enable mongod &>>$LOG
+Stat $?
+
+Print "Download Schema"
+curl -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongodb/archive/main.zip" &>>$LOG
+Stat $?
+
+Print "Extract Schema"
+unzip -o -d /tmp mongodb.zip &>>$LOG
+Stat $?
+
+Print "Load Schema"
+cd /tmp/mongodb-main
+mongo < catalogue.js &>>$LOG
+mongo < users.js &>>$LOG
+Stat $?
+
