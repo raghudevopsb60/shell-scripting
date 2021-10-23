2c61675f08da3df7a25d6a88890e6dbd87a7d583
Sriharsha Kalluru <skalluru@mac.local>
2021-10-23 07:46:27 +0530

    Update shell-scripting/

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -15,15 +15,17 @@ Print "Starting Nginx"
 systemctl start nginx &>>$LOG
 Stat $?
 
+Print "Download Html Pages"
+curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" &>>$LOG
+Stat $?
 
-exit
-curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
 
-cd /usr/share/nginx/html
-rm -rf *
-unzip /tmp/frontend.zip
-mv frontend-main/* .
-mv static/* .
-rm -rf frontend-master static README.md
-mv localhost.conf /etc/nginx/default.d/roboshop.conf
-systemctl restart nginx
+
+#cd /usr/share/nginx/html
+#rm -rf *
+#unzip /tmp/frontend.zip
+#mv frontend-main/* .
+#mv static/* .
+#rm -rf frontend-master static README.md
+#mv localhost.conf /etc/nginx/default.d/roboshop.conf
+#systemctl restart nginx
