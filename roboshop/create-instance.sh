c1aba2feac2db22ebc1aaa3f3217ab0919c48e30
Sriharsha Kalluru <skalluru@mac.local>
2021-10-23 08:27:41 +0530

    Update shell-scripting/

--- roboshop/create-instance.sh
+++ roboshop/create-instance.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-COUNT=$(aws ec2 describe-instances --filters  "Name=tag:Name,Values=$1" | jq ".Reservations[].Instances[].InstanceId" | wc -l)
+COUNT=$(aws ec2 describe-instances --filters  "Name=tag:Name,Values=$1" | jq ".Reservations[].Instances[].PrivateIpAddress" | grep -v null  | wc -l)
 
 if [ $COUNT -eq 0 ]; then
   aws ec2 run-instances --image-id ami-0e4e4b2f188e91845 --instance-type t3.micro --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$1}]" | jq
