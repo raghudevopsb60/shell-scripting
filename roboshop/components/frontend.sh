1d27635a207226b3ddb32b0eeb34075cfedbf53b
Sriharsha Kalluru <skalluru@mac.local>
2021-10-23 07:53:53 +0530

    Update shell-scripting/

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -6,15 +6,6 @@ Print "Installing Nginx"
 yum install nginx -y &>>$LOG
 Stat $?
 
-
-Print "Enabling Nginx"
-systemctl enable nginx &>>$LOG
-Stat $?
-
-Print "Starting Nginx"
-systemctl start nginx &>>$LOG
-Stat $?
-
 Print "Download Html Pages"
 curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" &>>$LOG
 Stat $?
@@ -28,10 +19,18 @@ unzip -o -d /tmp /tmp/frontend.zip &>>$LOG
 Stat $?
 
 Print "Copy files to Nginx path"
-mv /tmp/frontend-main/static/* /usr/share/nginx/html/.
+mv /tmp/frontend-main/static/* /usr/share/nginx/html/. &>>$LOG
 Stat $?
 
-#mv static/* .
-#rm -rf frontend-master static README.md
-#mv localhost.conf /etc/nginx/default.d/roboshop.conf
-#systemctl restart nginx
+Print "Copy Nginx Roboshop Config file"
+cp /tmp/frontend-main/localhost.conf /etc/nginx/default.d/roboshop.conf  &>>$LOG
+Stat $?
+
+Print "Enabling Nginx"
+systemctl enable nginx &>>$LOG
+Stat $?
+
+Print "Starting Nginx"
+systemctl restart nginx &>>$LOG
+Stat $?
+
