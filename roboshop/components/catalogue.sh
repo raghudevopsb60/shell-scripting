89654bcc729b622072e29e2180da40317091e86f
Sriharsha Kalluru <skalluru@mac.local>
2021-10-25 08:52:37 +0530

    Update shell-scripting/

--- roboshop/components/catalogue.sh
+++ roboshop/components/catalogue.sh
@@ -2,7 +2,7 @@
 
 source components/common.sh
 
-MSPACE=$(cat $0 | grep ^Print | awk -F '"' '{print $2}' | awk '{ print length }' | sort | tail -1)
+MSPACE=$(cat $0 common.sh | grep ^Print | awk -F '"' '{print $2}' | awk '{ print length }' | sort | tail -1)
 
 
 COMPONENT_NAME=Catalogue
