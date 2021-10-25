5d737d40fc97fc12000bab047fc8c487b6b87f5b
Sriharsha Kalluru <skalluru@mac.local>
2021-10-25 07:57:56 +0530

    Update shell-scripting/

--- roboshop/create-instance.sh
+++ roboshop/create-instance.sh
@@ -11,3 +11,4 @@ fi
 IP=$(aws ec2 describe-instances --filters  "Name=tag:Name,Values=$1" | jq ".Reservations[].Instances[].PrivateIpAddress" | grep -v null )
 ## xargs is used to remove the double  quotes
 sed -e "s/DNSNAME/$1.roboshop.internal/" -e "s/IPADDRESS/${IP}/" record.json >/tmp/record.json
+aws route53 change-resource-record-sets --hosted-zone-id Z0577679A6W027W86RBE --change-batch file:///tmp/record.json | jq
