13c74917c48bf2bfd9f27736778e408213ae7488
Sriharsha Kalluru <skalluru@mac.local>
2021-10-25 08:50:05 +0530

    Update shell-scripting/

--- roboshop/components/catalogue.sh
+++ roboshop/components/catalogue.sh
@@ -9,19 +9,6 @@ COMPONENT_NAME=Catalogue
 COMPONENT=catalogue
 NODEJS
 
-
-Print "Update DNS records in SystemD config"
-sed -i -e 's/MONGO_DNSNAME/mongodb.roboshop.internal/' /home/roboshop/catalogue/systemd.service   &>>$LOG
-Stat $?
-
-Print "Copy SystemD file"
-mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service
-Stat $?
-
-Print "Start Catalogue Service"
-systemctl daemon-reload &>>$LOG && systemctl restart catalogue &>>$LOG && systemctl enable catalogue &>>$LOG
-Stat $?
-
 Print "Checking DB Connections from APP"
 sleep 5
 STAT=$(curl -s localhost:8080/health  | jq .mongo)
