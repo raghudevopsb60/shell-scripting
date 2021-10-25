73eb46c428fde17ff6e3ca86a54590581bb2574f
Sriharsha Kalluru <skalluru@mac.local>
2021-10-25 08:38:07 +0530

    Update shell-scripting/

---
+++ roboshop/components/redis.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+source components/common.sh
+MSPACE=$(cat $0 | grep ^Print | awk -F '"' '{print $2}' | awk '{ print length }' | sort | tail -1)
+
+Print "Install Redis Repos"
+yum install yum-utils http://rpms.remirepo.net/enterprise/remi-release-7.rpm -y &>>$LOG
+Stat $?
+
+Print "Enable Redis Repos"
+yum-config-manager --enable remi &>>$LOG
+Stat $?
+
+Print "Install Redis"
+yum install redis -y &>>$LOG
+Stat $?
+
+
+Print "Update Redis Listen Address"
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/redis.conf &>>$LOG
+Stat $?
+
+
+Print "Start Redis Database"
+systemctl enable redis &>>$LOG && systemctl start redis &>>$LOG
