dafaaba24acaaf4f051cbae642d2eb44813114be
Sriharsha Kalluru <skalluru@mac.local>
2021-10-25 08:24:01 +0530

    Update shell-scripting/

--- roboshop/components/catalogue.sh
+++ roboshop/components/catalogue.sh
@@ -2,7 +2,7 @@
 
 source components/common.sh
 
-MLINE=$(cat $0 | grep ^Print | awk -F '"' '{print $2}' | awk '{ print length }' | sort | tail -1)
+MSPACE=$(cat $0 | grep ^Print | awk -F '"' '{print $2}' | awk '{ print length }' | sort | tail -1)
 
 
 
