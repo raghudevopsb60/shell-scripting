3f6f0e7e9b6885d49215e35fc981318246df6c34
Sriharsha Kalluru <skalluru@mac.local>
2021-10-25 08:23:12 +0530

    Update shell-scripting/

--- roboshop/components/common.sh
+++ roboshop/components/common.sh
@@ -1,5 +1,5 @@
 Print() {
-  LSPACE=$(echo $1 | awk '{length}')
+  LSPACE=$(echo $1 | awk '{print length}')
   echo $LSPACE
   echo -n -e "\e[1m$1${SPACES}\e[0m  ... "
   echo -e "\n\e[36m======================== $1 ========================\e[0m" >>$LOG
