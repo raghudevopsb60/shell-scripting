d04cfddb4e2445e6f2523edc7a8b5cc4ec04d198
Sriharsha Kalluru <skalluru@mac.local>
2021-10-25 08:54:02 +0530

    Update shell-scripting/

--- roboshop/components/user.sh
+++ roboshop/components/user.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 source components/common.sh
-MSPACE=$(cat $0 components/common.sh | grep ^Print | awk -F '"' '{print $2}' | awk '{ print length }' | sort | tail -1)
+MSPACE=$(cat $0 components/common.sh | grep Print | awk -F '"' '{print $2}' | awk '{ print length }' | sort | tail -1)
 
 COMPONENT_NAME=User
 COMPONENT=user
