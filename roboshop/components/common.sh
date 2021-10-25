d00e6bb4205b6e99f917484f373dd067336bb8b8
Sriharsha Kalluru <skalluru@mac.local>
2021-10-25 09:01:10 +0530

    Update shell-scripting/

--- roboshop/components/common.sh
+++ roboshop/components/common.sh
@@ -86,3 +86,16 @@ CHECK_MONGO_FROM_APP() {
     Stat 1
   fi
 }
+
+
+CHECK_REDIS_FROM_APP() {
+  Print "Checking DB Connections from APP"
+  sleep 5
+  STAT=$(curl -s localhost:8080/health  | jq .redis)
+  if [ "$STAT" == "true" ]; then
+    Stat 0
+  else
+    Stat 1
+  fi
+}
+
