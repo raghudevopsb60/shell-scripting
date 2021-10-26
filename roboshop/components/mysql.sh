0e22741cf9ec5e76d795e7b0c01b7b2f3fa48f71
Sriharsha Kalluru <skalluru@mac.local>
2021-10-26 08:02:55 +0530

    Update shell-scripting/

--- roboshop/components/mysql.sh
+++ roboshop/components/mysql.sh
@@ -2,7 +2,7 @@
 
 source components/common.sh
 
-MSPACE=$(cat $0 components/common.sh | grep ^Print | awk -F '"' '{print $2}' | awk '{ print length }' | sort | tail -1)
+MSPACE=$(cat $0 components/common.sh | grep Print | awk -F '"' '{print $2}' | awk '{ print length }' | sort | tail -1)
 
 
 COMPONENT_NAME=MySQL
