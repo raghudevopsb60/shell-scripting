be83b0b03d9e32cc0592e8548f6ad64d5323bb12
Sriharsha Kalluru <skalluru@mac.local>
2021-10-26 07:51:51 +0530

    Update shell-scripting/

--- roboshop/components/mysql.sh
+++ roboshop/components/mysql.sh
@@ -20,6 +20,15 @@ Print "Start MySQL Service"
 systemctl enable mysqld &>>$LOG && systemctl start mysqld &>>$LOG
 Stat $?
 
+DEFAULT_PASSWORD=$(grep 'temporary password' /var/log/mysqld.log | awk '{print $NF}')
+NEW_PASSWORD="RoboShop@1"
+
+echo 'show databases;' | mysql -uroot -p"${NEW_PASSWORD}"  &>>$LOG
+if [ $? -eq 0 ]; then
+  echo "Password is already changed"
+else
+  echo "Password is not changed"
+fi
 
 #
 #Now a default root password will be generated and given in the log file.
