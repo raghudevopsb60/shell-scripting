7c576eac5d29177e4a2225ea68ca69e8eaa6f4b8
Sriharsha Kalluru <skalluru@mac.local>
2021-10-26 07:40:11 +0530

    Update shell-scripting/

--- roboshop/create-instance.sh
+++ roboshop/create-instance.sh
@@ -8,6 +8,8 @@ else
   echo "Instance already exists"
 fi
 
+sleep 5
+
 IP=$(aws ec2 describe-instances --filters  "Name=tag:Name,Values=$1" | jq ".Reservations[].Instances[].PrivateIpAddress" | grep -v null )
 ## xargs is used to remove the double  quotes
 sed -e "s/DNSNAME/$1.roboshop.internal/" -e "s/IPADDRESS/${IP}/" record.json >/tmp/record.json
