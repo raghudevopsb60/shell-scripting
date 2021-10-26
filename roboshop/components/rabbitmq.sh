9f4dd484f5b3e4a58803d865299478dfec10aab1
Sriharsha Kalluru <skalluru@mac.local>
2021-10-26 08:33:55 +0530

    Update shell-scripting/

--- roboshop/components/rabbitmq.sh
+++ roboshop/components/rabbitmq.sh
@@ -25,6 +25,7 @@ Print "Start RabbitMQ"
 systemctl enable rabbitmq-server &>>$LOG && systemctl start rabbitmq-server &>>$LOG
 Stat $?
 
+Print
 # rabbitmqctl add_user roboshop roboshop123
 # rabbitmqctl set_user_tags roboshop administrator
 # rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"
