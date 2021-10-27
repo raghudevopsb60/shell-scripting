6219d3a366bc0be2dc393638977ea4ed9a042bfe
Sriharsha Kalluru <skalluru@mac.local>
2021-10-27 08:18:37 +0530

    Update shell-scripting/

--- roboshop/create-spot-instance.sh
+++ roboshop/create-spot-instance.sh
@@ -4,7 +4,7 @@ CREATE() {
   COUNT=$(aws ec2 describe-instances --filters  "Name=tag:Name,Values=$1" | jq ".Reservations[].Instances[].PrivateIpAddress" | grep -v null  | wc -l)
 
   if [ $COUNT -eq 0 ]; then
-    aws ec2 run-instances --launch-template LaunchTemplateId=lt-03d452b52cee20ce4,Version=1 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$1}]" "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=$1}]" | jq &>/dev/null
+    aws ec2 run-instances --launch-template LaunchTemplateId=lt-03d452b52cee20ce4,Version=2 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$1}]" "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=$1}]" | jq &>/dev/null
   else
     echo -e "\e[1;33m$1 Instance already exists\e[0m"
     return
